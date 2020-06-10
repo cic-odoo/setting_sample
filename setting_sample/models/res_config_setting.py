5a7f078d0e83046baf2d6b0a593c0831ffcb345f
cic-odoo <cic@odoo.com>
2020-06-10 12:35:49 -0700

    [ADD] screenshots and comment

--- setting_sample/models/res_config_setting.py
+++ setting_sample/models/res_config_setting.py
@@ -7,6 +7,10 @@ from odoo import api, fields, models, _
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
+
+    ##### How to get the field in another model #####
+    ### value = self.env['ir.config_parameter'].sudo().get_param('setting_sample.text_field') ###
+
     text_field = fields.Char(string='Text Field',
                              readonly=False,
                              config_parameter='setting_sample.text_field',
